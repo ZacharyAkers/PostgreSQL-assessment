33936e49c6225153660144f5ce0b3255d2bf0db9
ZacharyAkers <user@Users-MacBook-Air.local>
2017-06-27 20:15:58 -0400

    turn in version

--- todo.sql
+++ todo.sql
@@ -1,22 +1,19 @@
-CREATE TABLE todos(
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  details TEXT NULL,
-  priority INTEGER NOT NULL DEFAULT 1,
-  created_at DATE NOT NULL,
-  completed_at DATE NULL
+CREATE TABLE todos (
+id SERIAL PRIMARY KEY,
+title VARCHAR(225) NOT NULL,
+details TEXT NULL,
+priority INTEGER NOT NULL DEFAULT 1,
+created_at TIMESTAMP NOT NULL,
+completed_at TIMESTAMP NULL
 );
 
-INSERT INTO todos (title, details, priority, created_at) VALUES ('Play Guitar', 'Tune strings and play stairway to heaven', 1, DATE  '2017-06-27');
-INSERT INTO todos (title, details, priority, created_at) VALUES ('Drive home', 'Headbanging in traffic', 3, DATE '2017-06-20');
-INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Workout', 'Its good for you', 2, DATE '2017-06-27');
-INSERT INTO todos (title, details, priority, created_at) VALUES ('Do laundry', 'It Stanky', 1, DATE '2017-06-18');
-INSERT INTO todos (title, details, priority, created_at) VALUES ('Complete Project','Its due at 4 but isnt so bad', 4, DATE '2017-06-18', current_timestamp);
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Complete Project', 'Its due at 4 but isnt so bad', 5, '2017-06-27 2:00', '2017-06-27 4:00');
+INSERT INTO todos (title, priority, created_at) VALUES ('Drive home', 'Headbanging in traffic', 1, '2017-06-27 2:10');
+INSERT INTO todos(title, details, priority, created_at) VALUES ('Workout', 'Its good for you', 5, '2017-06-27 2:15');
+INSERT INTO todos(title, details, priority, created_at) VALUES ('Do laundry', 'It Stanky', 3, '2017-06-27 2:20');
+INSERT INTO todos(title, details, priority, created_at) VALUES ('Play Guitar', 'Tune strings and play stairway to heaven', 2, '2017-06-27 2:25');
 
-SELECT * FROM todos WHERE completed_at IS NULL;
-
+SELECT * FROM todos WHERE completed_at IS NOT NULL;
 SELECT * FROM todos WHERE priority > 1;
-
-UPDATE todos SET completed_at = current_timestamp WHERE id = 2;
-
+UPDATE todos SET completed_at = '2017-06-27 3:13' WHERE id = 3;
 DELETE FROM todos WHERE completed_at IS NOT NULL;
